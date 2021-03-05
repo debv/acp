f56b12d8253926b0f57b50276f2cef653521ce90
Deborah Venuti <deborahvenuti93@gmail.com>
2021-03-04 23:06:43 -0500

    comment

--- acp-on-change.sh
+++ acp-on-change.sh
@@ -6,11 +6,15 @@ REPO_NAME='Auto Commit and Push Test'
 REPO_DIRECTORY=~/Desktop/acp-test/auto-commit-and-push-test
 REMOTE_BRANCH_TO_PUSH_TO='master'
 #---------------------------------------------------------
-
 COMMIT_MESSAGE="auto-commit from $USER@$(hostname -s) on $(date)"
 GIT=`which git`
+
+# Add modified file(s) in specified directory, commit and push to repo
 cd ${REPO_DIRECTORY}
 ${GIT} add --all .
 ${GIT} commit -m "$COMMIT_MESSAGE"
-echo "$(${GIT} push -u origin ${REMOTE_BRANCH_TO_PUSH_TO})"
+GIT_PUSH_RESULT="$(${GIT} push -u origin ${REMOTE_BRANCH_TO_PUSH_TO})"
+echo "$GIT_PUSH_RESULT"
+
+# Display pop up notification on macOS to let you know it pushed to your repo
 osascript -e 'display notification "Pushed to repo: '"$REPO_NAME"'" with title "acp"'
