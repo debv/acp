2b384c420ea984c492e89853b13c08bea31a8f80
Deborah Venuti <deborahvenuti93@gmail.com>
2021-03-04 23:51:53 -0500

    comment

--- acp-on-change.sh
+++ acp-on-change.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
 #----Edit the values of lines 5-7 for your local usage----
-# I recommend making the DIRECTORY_TO_MONITOR be a directory in your repo
 REPO_NAME='Auto Commit and Push Test'
-REPO_DIRECTORY=~/Desktop/acp-test/auto-commit-and-push-test
+LOCAL_REPO_DIRECTORY=~/Desktop/acp-test/auto-commit-and-push-test
 REMOTE_BRANCH_TO_PUSH_TO='master'
 #---------------------------------------------------------
 COMMIT_MESSAGE="auto-commit from $USER@$(hostname -s) on $(date)"
 GIT=`which git`
 
 # Add modified file(s), commit and push to repo
-cd ${REPO_DIRECTORY}
+cd ${LOCAL_REPO_DIRECTORY}
 ${GIT} add --all .
 ${GIT} commit -m "$COMMIT_MESSAGE"
 GIT_PUSH_RESULT="$(${GIT} push -u origin ${REMOTE_BRANCH_TO_PUSH_TO})"
